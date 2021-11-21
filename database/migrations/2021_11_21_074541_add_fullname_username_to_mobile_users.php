c5a034b8fe2ca3b7aabc1b416a7e41d6d0488d5d
chetan <chetan@tecmavin.com>
2021-11-21 13:39:00 +0530

    fixed untraked files

---
+++ database/migrations/2021_11_21_074541_add_fullname_username_to_mobile_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFullnameUsernameToMobileUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mobile_users', function (Blueprint $table) {
+            //
+            $table->dropColumn('last_name');
+            $table->dropColumn('picture');
+            $table->renameColumn('first_name', 'full_name');
+            $table->string('user_name',200)->after('first_name');
+            $table->string('profile_picture',200)->after('phone_no')->nullable()->change();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mobile_users', function (Blueprint $table) {
+            //
+        });
+    }
+}
